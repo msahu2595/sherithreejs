1fc06adcda28947eddb252b2d248f9b6038a1030
Manish Kumar Sahu <msahu2595@gmail.com>
2024-07-17 16:45:32 +0530

    [ADD] Add wave transition on images using ThreeJS & GSAP  lib.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-export default {
+import glsl from "vite-plugin-glsl";
+import { defineConfig } from "vite";
+
+export default defineConfig({
   server: {
     host: "0.0.0.0", // or 'localhost'
     port: 5173,
   },
-};
+  plugins: [glsl()],
+});
